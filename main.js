a97c7b5695f8d3c653fb02a765b4188f274d4cc7
Nico <nicodiazan@gmail.com>
2022-11-19 15:55:18 +0100

    Finish initial styling

--- main.js
+++ main.js
@@ -1,6 +1,81 @@
 
 let lastId = 0;
 
+function setup() {
+    const projects = [];
+    const users = [];
+    // Announcements are stored as text bundles
+    const announcements = [];
+
+    for (let i = 0; i < 6; i++) {
+        randomProject(projects);
+    }
+
+    for (let i = 0; i < 3; i++) {
+        randomAnnouncement(announcements);
+    }
+
+    for (let i = 0; i < 4; i++) {
+        randomUser(users);
+    }
+
+    populateDashboard(projects, announcements, users);
+}
+
+function randomProject(projectArr) {
+    const title = randomStr(Math.floor(Math.random() * 14)) + " - " + lastId;;
+    let description = "";
+    
+    for (let i = 0; i < Math.floor(Math.random() * 20); i++) {
+        description += randomStr(Math.floor(Math.random() * 14)) + " ";
+    }
+
+    projectArr.push(new Project(title, description, false, false));
+
+    return projectArr;
+}
+
+function randomAnnouncement(announcementArr) {
+    let title = "";
+    let description = "";
+    
+    for (let i = 0; i < Math.floor(Math.random() * 3); i++) {
+        title += randomStr(Math.floor(Math.random() * 14)) + " ";
+    }
+    for (let i = 0; i < Math.floor(Math.random() * 20); i++) {
+        description += randomStr(Math.floor(Math.random() * 14)) + " ";
+    }
+
+    announcementArr.push(new TextBundle(title + " - " + lastId, description));
+
+    return announcementArr;
+}
+
+function randomUser(userArr) {
+    const title = "@" + randomStr(Math.floor(Math.random() * 10) + 4).slice(1) + " - " + lastId;;
+    let description = "";
+    
+    for (let i = 0; i < (Math.floor(Math.random() * 2) + 2); i++) {
+        description += randomStr(Math.floor(Math.random() * 10)) + " ";
+    }
+
+    userArr.push(new TrendingUser(title, description));
+
+    return userArr;
+}
+
+const randomStr = (length = 8, id) => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    let str = '';
+    str += chars.charAt(Math.floor(Math.random() * chars.length));
+    for (let i = 1; i < length; i++) {
+        str += chars.charAt(Math.floor(Math.random() * chars.length)).toLowerCase();
+    }
+
+    return str;
+};
+
 const TextBundle = function(
     title,
     description
@@ -29,7 +104,7 @@ const TrendingUser = function(
     id
 ) {
     this.bundle = new TextBundle(tag, position);
-    this.pfpUrl = typeof(pfpUrl) === "string" ? pfpUrl : "./icons/user.png";
+    this.pfpUrl = typeof(pfpUrl) === "string" ? pfpUrl : `./icons/user.png`;
     this.id = typeof(id) === "number" ? id : newId();
 }
 
@@ -48,8 +123,8 @@ function generateProject(project) {
                 <p class="project-desc">${project.bundle.description}</p>
             </div>
             <div class="project-utils">
-                <button class="btn-icon star ${project.isStarred ? "starred" : "not-starred"}"></button>
-                <button class="btn-icon watch ${project.isWatched ? "watched" : "not-watched"}"></button>
+                <button class="btn-icon star ${project.isStarred ? "not-starred" : "starred"}"></button>
+                <button class="btn-icon watch ${project.isWatched ? "not-watched" : "watched"}"></button>
                 <button class="btn-icon fork"></button>
             </div>
         </div>
@@ -101,15 +176,13 @@ function generateUsers(users, usersCount) {
         const thisUser = users[i];
         const thisUserHTML = `
         <div id="${thisUser.id}" class="user-cell">
-            <div class="user-pfp" style="background: transparent url("${thisUser.pfpUrl}") 
-            no-repeat fixed center;"></div>
+            <div class="user-pfp" style="background-color: transparent; background-image: url('${thisUser.pfpUrl}');"></div>
             <div class="user-text">
                 <div class="user-tag">${thisUser.bundle.title}</div>
                 <div class="user-position">${thisUser.bundle.description}</div>
             </div>
         </div>
         `;
-
         usersHTML += thisUserHTML;
     }
 
@@ -121,3 +194,20 @@ function generateUsers(users, usersCount) {
 function newId () {
     return lastId++;
 }
+
+function populateDashboard (projectsArr, announcementsArr, usersArr) {
+    const projectsCanvas = document.getElementById("projects-canvas");
+    const announcementsCanvas = document.getElementById("announcements-wrapper");
+    const usersCanvas = document.getElementById("trending-users-wrapper");
+
+    for (let i = 0; i < 6; i++) {
+        const project = projectsArr[i];
+        projectsCanvas.insertAdjacentHTML("beforeend", generateProject(project));
+    }
+
+    announcementsCanvas.insertAdjacentHTML("beforeend",generateAnnouncements(announcementsArr, 3));
+    console.log(usersArr);
+    usersCanvas.insertAdjacentHTML("beforeend",generateUsers(usersArr, 4));
+}
+
+setup();
